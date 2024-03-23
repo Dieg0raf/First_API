479a33682493642dbbbf345471dc235014d8ac5f
diegoo <drafael038@insite.4cd.edu>
2024-03-23 16:03:34 -0700

    Created ProductListCreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,3 +2,22 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+
+# DetailView -> Get one single item
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # lookup_field = 'pk'
